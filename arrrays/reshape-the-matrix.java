1e654abc10ce74cfeab3b243d023c470be16fcf3
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-04-10 15:39:45 +0530

    Update reshape-the-matrix.java

--- arrrays/reshape-the-matrix.java
+++ arrrays/reshape-the-matrix.java
@@ -2,23 +2,25 @@ class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         int m = mat.length;
         int n = mat[0].length;
-        if(m * n != r * c)
+        if (m * n != r * c)
             return mat;
-        else if(m == r && n == c)
-            return mat;    
-        int[][] result = new int[r][c];
-        int newRow = 0; 
-        int newCol = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                result[newRow][newCol] = mat[i][j];
-                newCol++;
-                if(newCol == c){
-                    newCol = 0;
-                    newRow++;
+        else if (m == r && n == c)
+            return mat;
+        else {
+            int[][] result = new int[r][c];
+            int newRow = 0;
+            int newCol = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    result[newRow][newCol] = mat[i][j];
+                    newCol++;
+                    if (newCol == c) {
+                        newCol = 0;
+                        newRow++;
+                    }
                 }
             }
+            return result;
         }
-        return result;
     }
 }
