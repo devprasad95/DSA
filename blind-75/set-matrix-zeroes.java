213d6a63a55fcbc3ab7fddbe57a485804b2746d7
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-04-16 20:17:40 +0530

    Update set-matrix-zeroes.java

--- blind-75/set-matrix-zeroes.java
+++ blind-75/set-matrix-zeroes.java
@@ -4,12 +4,15 @@ class Solution {
         int n = matrix[0].length;
         boolean rowZero = false;
         boolean colZero = false;
+        //Check whether first row and col have any zero
         for(int i = 0; i < n; i++){
-            if(matrix[0][i] == 0) rowZero = true;
+            if(matrix[0][i] == 0) rowZero = true; 
         }
         for(int i = 0; i < m; i++){
             if(matrix[i][0] == 0) colZero = true;
         }
+        // we will store this value for future refrence
+        // Next we will mark 0 value in 0th row and col
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(matrix[i][j] == 0){
@@ -18,6 +21,7 @@ class Solution {
                 }
             }
         }
+        // then based on the zero value in 0th row and zero we will make values from index i to zero
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(matrix[i][0] == 0 || matrix[0][j] == 0){ 
@@ -25,6 +29,7 @@ class Solution {
                 }
             }
         }
+        // now we can update first row and col
         if(rowZero){
             for(int i = 0; i < n; i++){
                 matrix[0][i] = 0;
