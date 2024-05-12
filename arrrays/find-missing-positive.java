040852cd39f17e1642de27c76affd302ed0b6d71
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-05-12 18:25:09 +0530

    Create find-missing-positive.java

---
+++ arrrays/find-missing-positive.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        if(nums == null || n == 0){
+            return 1;
+        }
+        
+        boolean containsOne = false;
+      //step 1
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 1) {
+                containsOne = true;
+            } else if(nums[i] <= 0 || nums[i] > n){
+                nums[i] = 1;
+            }
+        }
+        if(!containsOne){
+            return 1;
+        }
+      //step 2
+        for(int i = 0; i < n; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] > 0){
+                nums[index] = -1 * nums[index];
+            }
+        }
+      //step 3
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                return i + 1;
+            }
+        }
+        return n + 1; // [1,2,3]
+    }
+}
