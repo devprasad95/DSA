a54479f3185c5935e665601e39aed96f32c4ebe7
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-05-19 01:05:15 +0530

    Update search-in-rotated-sorted-array.java

--- binary-search/search-in-rotated-sorted-array.java
+++ binary-search/search-in-rotated-sorted-array.java
@@ -1,47 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = pivot(nums);
-        int n = nums.length;
-        if(pivot == -1){
-            return binarySearch(nums,target, 0, n - 1);
-        }
-        if(nums[pivot] == target){
-            return pivot;
-        }
-        if(nums[0] <= target){
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-        return binarySearch(nums, target, pivot + 1, n - 1);
-    }
-    public int binarySearch(int[] arr, int target, int start, int end){
+        int start = 0; 
+        int end = nums.length - 1;
         while(start <= end){
             int mid = start + (end - start) / 2;
-            if(target < arr[mid]){
-                end = mid - 1;
-            } else if(target > arr[mid]){
-                start = mid + 1;
-            } else{
+            if(nums[mid] == target){
                 return mid;
             }
-        }
-        return -1;
-    }
-    public int pivot(int[] arr){
-        int start = 0;
-        int end = arr.length - 1;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(mid < end && arr[mid] > arr[mid + 1]){
-                return mid;
-            }
-            if(start < mid && arr[mid - 1] > arr[mid]){
-                return mid - 1;
-            }
-            if(arr[start] >= arr[mid]){
-                end = mid - 1;
-            } 
-            else{
-                start = mid + 1;
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if(nums[mid] <= target && target <= nums[end]){
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
         return -1;
