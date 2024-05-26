992d78987422f4d6244ff1d5517cfbb8687c4ba1
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-05-26 20:07:35 +0530

    Create group-anagrams.java

---
+++ blind-75/group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            if(!map.containsKey(sortedWord)){
+                map.put(sortedWord, new ArrayList<>());
+            }
+            map.get(sortedWord).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
