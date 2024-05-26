524b2d7ce270aa4c860c58e71d6496c94ff220a9
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-05-27 00:40:49 +0530

    Update find-in-mountain-array.java

--- binary-search/find-in-mountain-array.java
+++ binary-search/find-in-mountain-array.java
@@ -10,12 +10,12 @@ class Solution {
     private int peakIndexInMountainArray(MountainArray arr){
         int start = 0;
         int end = arr.length() - 1;
-        while(start < end){
+        while(start <= end){
             int mid = start + (end -start) / 2;
             if(arr.get(mid) < arr.get(mid + 1)){
                 start = mid + 1;
             } else{
-                end = mid;
+                end = mid - 1;
             }
         }
         return start;
