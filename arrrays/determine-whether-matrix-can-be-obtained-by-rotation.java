cf4c9f979ffcf11beb4c077aa46bc8fc290d08c4
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-06-01 03:14:35 +0530

    Update determine-whether-matrix-can-be-obtained-by-rotation.java

--- arrrays/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ arrrays/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,3 +1,4 @@
+// 1st method
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         int n = mat.length;
@@ -21,6 +22,44 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (a[i][j] != b[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+//2nd method
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        if(equals(mat,target)){
+            return true;
+        }
+        int n = mat.length;
+        int[][] temp = new int[n][n];
+        for(int k = 0; k < 3; k++){
+            
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    temp[i][j] = mat[j][n-1-i];
+                }
+            }
+            if(equals(temp,target)){
+                return true;
+            }
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    mat[i][j] = temp[i][j];
+                }
+            }
+        }
+        return false;
+    }
+    public boolean equals(int[][] a, int[][] b){
+        int n = a.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(a[i][j] != b[i][j]){
                     return false;
                 }
             }
