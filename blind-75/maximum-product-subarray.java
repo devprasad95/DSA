64a88ad9b7b1f890dde62bb84a2719a27e03a88b
Dev Prasad <99073413+devprasad95@users.noreply.github.com>
2024-07-05 21:29:15 +0530

    Update maximum-product-subarray.java

--- blind-75/maximum-product-subarray.java
+++ blind-75/maximum-product-subarray.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int preffix = 1, suffix = 1, n = nums.length;
-        if(n == 21){
-            return 1000000000; // for last edge case
-        }
-        int ans = Integer.MIN_VALUE;
+        int n = nums.length;
+        double left = 1;
+        double right = 1;
+        int max = Integer.MIN_VALUE;
         for(int i = 0; i < n; i++){
-            if(preffix == 0) preffix = 1;
-            if(suffix == 0) suffix = 1;
-            preffix *= nums[i];
-            suffix *= nums[n-1-i];
-            ans = Math.max(ans, Math.max(preffix, suffix));
+            if(left == 0) left = 1;
+            if(right == 0) right = 1;
+            left *= (double) nums[i];
+            right *= (double) nums[n - 1 - i];
+            max = (int) Math.max((double) max, Math.max(left, right));
         }
-        return ans;
+        return max;
     }
 }
